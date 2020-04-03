dd716531ad31d8564ab02b54ce9348e5449e9c65
Guangyao Miao <guangyaomiao@Guangyaos-iMac.fios-router.home>
2020-04-02 21:52:39 -0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,71 @@
 import React, { Component, ChangeEvent } from 'react';
 import './App.css';
-import { COLUMN_INDEX } from './Consts';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
+import H1bTable from './h1bTable';
 
 class App extends Component {
   state = {
-    companyNames: [], 
+    companyNames: [],
     searchResult: []
   }
 
   componentDidMount() {
     fetch('/getCompanyNames')
       .then(res => res.json())
-      .then(companyNames => this.setState({ companyNames }));
+      .then(companyNames => { 
+        companyNames = companyNames.filter((name: Array<any>) => {
+          if (!name || name.length == 0) {
+            return false
+          }
+          if (typeof name[0] == "string" && name[0].length > 0) {
+            return true
+          }
+          return false
+        }).map((name: Array<String>) => {
+            return name[0]
+        }).slice(0, 2000)
+        this.setState({ companyNames })
+      });
   }
 
   onNameSearchChange = (event: ChangeEvent<{}>, val: any) => {
     console.log(val)
     if (!val) {
-      return this.setState({searchResult: []})
+      return this.setState({ searchResult: [] })
     }
-    let name = val[0]
+    let name = val
     fetch('/getRecordByCompanyName?' + new URLSearchParams({ name }))
       .then(res => res.json())
       .then(searchResult => {
-        console.log(searchResult); 
+        console.log(searchResult);
         return this.setState({ searchResult })
       });
   }
   render() {
     return (
       <div className="App">
-        <h1>Search</h1>
-        <Autocomplete
-          id="company-name-box"
-          options={this.state.companyNames}
-          getOptionLabel={(option) => option[0]}
-          style={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Company Name" variant="outlined" />}
-          onChange={this.onNameSearchChange}
-        />
-        {this.state.searchResult.map((item, key) => <li key={item[0]}>
-          {item[COLUMN_INDEX['JOB_TITLE']]} | {item[COLUMN_INDEX['EMPLOYER_NAME']]} | {item[COLUMN_INDEX['PREVAILING_WAGE_1']]} | {item[COLUMN_INDEX['PW_UNIT_OF_PAY_1']] || "Year"}  </li>)}
+        <h1>H1b Data Analysis</h1>
+        <Grid container spacing={3}>
+          <Grid item xs >
+          </Grid>
+          <Grid item xs={6}>
+            <Autocomplete 
+              size="small"
+              id="company-name-box"
+              options={this.state.companyNames}
+              getOptionLabel={option => option}
+              style={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Company Name" variant="outlined" />}
+              onChange={this.onNameSearchChange}
+            />
+            <H1bTable searchResult={this.state.searchResult} />
+          </Grid>
+          <Grid item xs >
+          </Grid>
+        </Grid>
       </div>
     );
   }
---
+++ src/h1bTable.tsx
@@ -0,0 +1,232 @@
+import React, { Component } from 'react';
+import Box from '@material-ui/core/Box';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TablePagination from "@material-ui/core/TablePagination";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Paper from '@material-ui/core/Paper';
+import { COLUMN_INDEX } from './Consts';
+import {
+  createStyles,
+  lighten,
+  makeStyles,
+  Theme
+} from "@material-ui/core/styles";
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+interface Data {
+  id: string;
+  jobTitle: string;
+  employerName: string;
+  wage: number;
+}
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+interface EnhancedTableProps {
+  classes: ReturnType<typeof useStyles>;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+const headCells: HeadCell[] = [
+  { id: "jobTitle", numeric: false, disablePadding: false, label: "Job Title" },
+  { id: "employerName", numeric: false, disablePadding: false, label: "Employer Name" },
+  { id: "wage", numeric: true, disablePadding: false, label: "Wage" },
+];
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property: keyof Data) => (
+    event: React.MouseEvent<unknown>
+  ) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "default"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+// const handleChangePage = (event: unknown, newPage: number) => {
+//   setPage(newPage);
+// };
+
+// const handleChangeRowsPerPage = (
+//   event: React.ChangeEvent<HTMLInputElement>
+// ) => {
+//   setRowsPerPage(parseInt(event.target.value, 10));
+//   setPage(0);
+// };
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%"
+    },
+    paper: {
+      width: "100%",
+      marginBottom: theme.spacing(2)
+    },
+    table: {
+      minWidth: 750
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: "rect(0 0 0 0)",
+      height: 1,
+      margin: -1,
+      overflow: "hidden",
+      padding: 0,
+      position: "absolute",
+      top: 20,
+      width: 1
+    }
+  })
+);
+
+const H1bTable = (params: { searchResult: any[]; }) => {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState<keyof Data>("wage");
+  const [selected] = React.useState<string[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDense(event.target.checked);
+  };
+
+  const formattedResult = params.searchResult.map((item: any, key) => {
+    return {
+      id: key,
+      jobTitle: item[COLUMN_INDEX["JOB_TITLE"]],
+      employerName: item[COLUMN_INDEX['EMPLOYER_NAME']],
+      wage: item[COLUMN_INDEX['PREVAILING_WAGE_1']],
+    }
+  })
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, formattedResult.length - page * rowsPerPage);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table size="small">
+        <colgroup>
+          <col width="50%" />
+          <col width="20%" />
+          <col width="30%" />
+        </colgroup>
+        <EnhancedTableHead
+          classes={classes}
+          order={order}
+          orderBy={orderBy}
+          onRequestSort={handleRequestSort}
+          rowCount={formattedResult.length}
+        />
+        <TableBody>
+          {stableSort(formattedResult, getComparator(order, orderBy))
+                // .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow tabIndex={-1} key={row.id}>
+                      <TableCell component="th" scope="row" align="left"> {row.jobTitle} </TableCell>
+                      <TableCell align="left">{row.employerName}</TableCell>
+                      <TableCell align="right">{row.wage}</TableCell>
+                    </TableRow>
+                  );
+            })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default H1bTable
